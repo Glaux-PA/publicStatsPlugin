4423c7ade9399dd92e5b427168a6e29fc8182ba3
leofa98 <leonarfer93@gmail.com>
2026-09-22 14:35:54 +0200

    quitar el campo email de los datos de autor, que no se usaba

--- services/AuthorStatsService.php
+++ services/AuthorStatsService.php
@@ -52,7 +52,6 @@ class AuthorStatsService extends BaseStatsService
         $authorData = [
             'id'          => $authorIds[0],
             'fullName'    => $entry['name'],
-            'email'       => $entry['email'] ?? null,
             'affiliation' => $entry['affiliation'] ?? null,
             'country'     => $entry['country'] ?? null,
             'orcid'       => $entry['orcid'] ?? null,
@@ -193,7 +192,6 @@ class AuthorStatsService extends BaseStatsService
                     'key'             => $key,
                     'ids'             => implode(',', $authorData['ids']),
                     'name'            => $authorData['name'],
-                    'email'           => $authorData['email'],
                     'affiliation'     => $authorData['affiliation'],
                     'orcid'           => $authorData['orcid'],
                     'publicationCount' => $publicationCount
